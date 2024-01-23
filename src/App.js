1e7da5cdf4d6c9085d57d2f53197c3309543c9cd
khwww <khw4420@knu.ac.kr>
2024-01-24 01:04:32 +0900

    상단바 및 대문사진 제작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
+import {Button, Navbar, Container, Nav, Form, FormControl} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaSearch } from 'react-icons/fa';
 import './App.css';
 
-function App() {
+function App(){
   return (
     <div className="App">
-      쇼핑몰 제작
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container className="justify-content-between">
+        <Navbar.Brand href="#home">Samgak Store</Navbar.Brand>
+    
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#products">Products</Nav.Link>
+            <Nav.Link href="#categories">Categories</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>    
+        <Form inline className="mx-auto">
+          <Button variant="outline-light"><FaSearch /></Button>
+        </Form>
+      </Container>
+      </Navbar>
+      
+      <div className="main-bg"></div>
+      
+
+
     </div>
   );
 }
