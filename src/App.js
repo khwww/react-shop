f640725a7d3592a0ceb8c06dbdc626c01ca010e8
khwww <khw4420@knu.ac.kr>
2024-01-26 04:41:38 +0900

    Card컴포넌트로 데이터 바인딩, map사용

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App(){
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#products">Products</Nav.Link>
-            <Nav.Link href="#categories">Categories</Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link href="#home">Porter</Nav.Link>
+            <Nav.Link href="#products">Polo</Nav.Link>
+            <Nav.Link href="#categories">Sweat</Nav.Link>
+            <Nav.Link href="#cart">old-shoes</Nav.Link>
           </Nav>
         </Navbar.Collapse>    
         <Form inline className="mx-auto">
@@ -32,43 +32,27 @@ function App(){
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-          <img src={require("./img/bag1.jpg")} width="80%" />
-            <h4>{ bag[0].title }</h4>
-            <p>{ bag[0].price }</p>
-          </div>
-          <div className="col-md-4">
-          <img src={require("./img/bag1.jpg")} width="80%" />
-            <h4>{ bag[1].title }</h4>
-            <p>{ bag[1].price }</p>
-          </div>
-          <div className="col-md-4">
-          <img src={require("./img/bag1.jpg")} width="80%" />
-            <h4>{ bag[2].title }</h4>
-            <p>{ bag[2].price }</p>
-          </div>
-          <div className="col-md-4">
-          <img src={require("./img/bag1.jpg")} width="80%" />
-            <h4>{ bag[3].title }</h4>
-            <p>{ bag[3].price }</p>
-          </div>
-          <div className="col-md-4">
-          <img src={require("./img/bag1.jpg")} width="80%" />
-            <h4>{ bag[4].title }</h4>
-            <p>{ bag[4].price }</p>
-          </div>
-          <div className="col-md-4">
-          <img src={require("./img/bag1.jpg")} width="80%" />
-            <h4>{ bag[5].title }</h4>
-            <p>{ bag[5].price }</p>
-          </div>
+          {/* <Card bag={bag[1]} i={2}></Card> */}
+          {
+            bag.map((a, i)=>{
+              return (
+                <Card key={i} bag={bag[i]} i={i}></Card>
+              )
+            })
+          }
         </div>
       </div>
-      
-
-
     </div>
   );
+}
+function Card(props){
+  return (
+    <div className='col-md-4'>
+      <img src={require('./img/bag' + (props.i) + '.jpg')} width="80%" />
+      <h4>{ props.bag.title }</h4>
+      <p>{ props.bag.price }</p>
+    </div>
+  )
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -40,7 +40,225 @@ let data = [ //대괄호니까 array
     title : "porter half bag[olive]",
     content : "Born in the States",
     price : 130000
+  },
+  
+  {
+    id : 6,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 7,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 8,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 9,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 10,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 11,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 12,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 13,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 14,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 15,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+
+  {
+    id : 16,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 17,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 18,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 19,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 20,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 21,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 22,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 23,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 24,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 25,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 26,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 27,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 28,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 29,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 30,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 31,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 32,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 33,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 34,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 35,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
+  },
+  
+  {
+    id : 36,
+    title : "porter half bag[olive]",
+    content : "Born in the States",
+    price : 130000
   }
+
 ] 
 
 export default data;
