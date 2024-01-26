efc937fc990b732406fb59971e432179361d4d25
khwww <khw4420@knu.ac.kr>
2024-01-27 06:09:23 +0900

    이미지/상품명/가격 클릭 시 해당 디테일 페이지로 기능 구현

--- src/App.js
+++ src/App.js
@@ -100,11 +100,16 @@ function App(){
   );
 }
 function Card(props){
+  let navigate = useNavigate();
+  let handleClick = () => {
+    // 이미지 클릭 시 해당 상품의 디테일 페이지로 이동
+    navigate(`/detail/${props.i}`);
+  };
   return (
     <div className='col-md-4'>
-      <img src={require('./img/bag' + (props.i) + '.jpg')} width="80%" />
-      <h4>{ props.bag.title }</h4>
-      <p>{ props.bag.price }</p>
+      <img onClick={handleClick} src={require('./img/bag' + (props.i) + '.jpg')} width="80%" />
+      <h4 onClick={handleClick}>{ props.bag.title }</h4>
+      <p onClick={handleClick}>{ props.bag.price }</p>
     </div>
   )
 }
