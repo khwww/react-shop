e16cf971ba918e4782e8f2457671ab7d9a8f62c2
khwww <khw4420@knu.ac.kr>
2024-01-28 04:34:20 +0900

    tap 버튼 클릭시 버튼에 맞는 화면 표시 기능

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { Nav } from 'react-bootstrap'
+import { func } from 'prop-types';
 
 // 이거 styeld component
 // let YelloBtn = styled.button`
@@ -20,6 +21,9 @@ function Detail(props) {
     return x.id == id
   });
 
+  let[탭, 탭변경] = useState(0)
+
+
   if(!id) {
     return <div>기본 디테일 페이지 입니다?</div>
   }
@@ -27,6 +31,8 @@ function Detail(props) {
   if(!찾은상품) {
     return <div>상품을 찾을 수 없습니다.</div>
   }
+
+  
 
 
   
@@ -49,25 +55,50 @@ function Detail(props) {
     {/* defaultActiveKey는 페이지에 들어갔을 때 기본적으로 선택되어 있을 탭을 의미 */}
     <Nav variant="tabs" defaultActiveKey="link0">
       <Nav.Item>
-        <Nav.Link eventKey="link0">버튼0</Nav.Link>
+        <Nav.Link onClick={()=>{ 탭변경(0) }} eventKey="link0">버튼0</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link1">버튼1</Nav.Link>
+        <Nav.Link onClick={()=>{ 탭변경(1) }} eventKey="link1">버튼1</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link2">버튼2</Nav.Link>
+        <Nav.Link onClick={()=>{ 탭변경(2) }} eventKey="link2">버튼2</Nav.Link>
       </Nav.Item>
     </Nav>
-    <div>내용0</div>
-    <div>내용1</div>
-    <div>내용2</div>
 
-
-
+    <TapContent 탭={탭}/>
 
   </div> 
   )
 
+
+}
+
+function TapContent(props) {
+  if(props.탭 == 0){
+    return <div>
+      포터(PORTER)는 1935년 가방 장인인 요시다 기치조가 설립한 요시다 컴퍼니에서 1962년 발표한 자체 브랜드로<br/>
+“바늘 한 땀 한 땀에 영혼을 담는다”라는 ‘일침입혼(一針入魂)’의 장인 정신을 바탕으로 하고 있습니다.<br/>
+현대적인 디자인에 뛰어난 기술력과 기능성, 사용하면 할수록 감탄을 더하게 하는 내구성과 실용성을 모두 겸비한 가방 브랜드로 잘 알려져 있습니다.<br/>
+여러 브랜드와의 컬래버레이션을 통해서 다채로운 행보를 이어가고 있으며 무엇보다도 물건을 운반하는 도구라는 가방의 기본적인 본질을 우선으로 하여<br/>
+모든 제작 과정에 숙련된 장인들의 눈과 손을 거쳐 탄생합니다.
+포터(PORTER)는 1935년 가방 장인인 요시다 기치조가 설립한 요시다 컴퍼니에서 1962년 발표한 자체 브랜드로<br/>
+“바늘 한 땀 한 땀에 영혼을 담는다”라는 ‘일침입혼(一針入魂)’의 장인 정신을 바탕으로 하고 있습니다.<br/>
+현대적인 디자인에 뛰어난 기술력과 기능성, 사용하면 할수록 감탄을 더하게 하는 내구성과 실용성을 모두 겸비한 가방 브랜드로 잘 알려져 있습니다.<br/>
+여러 브랜드와의 컬래버레이션을 통해서 다채로운 행보를 이어가고 있으며 무엇보다도 물건을 운반하는 도구라는 가방의 기본적인 본질을 우선으로 하여<br/>
+모든 제작 과정에 숙련된 장인들의 눈과 손을 거쳐 탄생합니다.
+포터(PORTER)는 1935년 가방 장인인 요시다 기치조가 설립한 요시다 컴퍼니에서 1962년 발표한 자체 브랜드로<br/>
+“바늘 한 땀 한 땀에 영혼을 담는다”라는 ‘일침입혼(一針入魂)’의 장인 정신을 바탕으로 하고 있습니다.<br/>
+현대적인 디자인에 뛰어난 기술력과 기능성, 사용하면 할수록 감탄을 더하게 하는 내구성과 실용성을 모두 겸비한 가방 브랜드로 잘 알려져 있습니다.<br/>
+여러 브랜드와의 컬래버레이션을 통해서 다채로운 행보를 이어가고 있으며 무엇보다도 물건을 운반하는 도구라는 가방의 기본적인 본질을 우선으로 하여<br/>
+모든 제작 과정에 숙련된 장인들의 눈과 손을 거쳐 탄생합니다.
+    </div>
+  }
+  else if(props.탭 == 1){
+    return <div>이 가방 실물이 더 이뻐요. 저는 거짓말을 하지 않습니다. i'm so sorry but i love you.</div>
+  }
+  else if(props.탭 == 2){
+    return <div>이 가방 팔아도 남는 게 없습니다. 진짜에요. 저 그냥 자선사업가라고 보시면 됩니다. i'm so sorry but i love you.</div>
+  }
 }
 
 export default Detail;
