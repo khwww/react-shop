a9648a602965f5f4573c17cd60223972138e594b
khwww <khw4420@knu.ac.kr>
2024-02-17 21:08:30 +0900

    이벤트 페이지 문구 수정

--- src/routes/Event.js
+++ src/routes/Event.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function Event() {
   return (
     <div>
-      <h4>오늘의 이벤트</h4>
+      <h4>진행중인 이벤트가 없습니다.</h4>
       <Outlet></Outlet>
     </div>
   )
