fbbb06076b9507b84d569fb539f351f2a6cc1b62
khwww <khw4420@knu.ac.kr>
2024-02-18 00:51:53 +0900

    상품 데이터 입력

--- src/data.js
+++ src/data.js
@@ -2,258 +2,258 @@ let data = [ //대괄호니까 array
 //지금 하나의 array안에 6개의 object정보들이 들어가 있는 거임
   {
     id : 0,
-    title : "porter leather bag[brown]",
-    content : "Born in France",
+    title : "Porter girl bonbon shoulder bag s[black]",
+    content : "•Detail : 애나멜로 되어있는 포터의 숄더백입니다. •Size : 가로29 세로15",
     price : 120000
   },
 
   {
     id : 1,
-    title : "porter tanker bag[olive]",
-    content : "Born in Seoul",
+    title : "porter leather shoulder bag(brown)",
+    content : "포터의 브라운 숄더백입니다. 가죽 상태 우수하고 에이징도 예쁜 주름으로 잡혀있어 더욱 매력적인 숄더백입니다. 포터의 로고가 음각으로 박혀있는 희귀한 가방입니다\nSize : 가로25 세로16",
     price : 110000
   },{
     id : 2,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "porter tanker shoulder bag (olive)",
+    content : "•Detail : 요시다 포터의 탱커 숄더백 카키색상입니다. 특별한 하자 없는 우수한 컨디션 입니다. •Size : 가로26 세로18",
     price : 130000
   },
 
   {
     id : 3,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter half moon shoulder bag(sage green)",
+    content : "•Detail : 포터의 반달 하프문 숄더백 세이지 그린 색상입니다. 특별한 하자 없으며 전체적으로 상태 우수합니다. •Size : 가로34 세로20",
     price : 130000
   },
 
   {
     id : 4,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter x atmos 2-way boston bag",
+    content : "•Detail : 일본의 편집샵 아트모스와 포터의 콜라보 보스턴 원통백 2-way 제품입니다. 사용감 거의 존재하지 않는 데드스탁급 제품입니다. 제가 쓰고 싶… •Size : 가로35 세로20",
     price : 130000
   },
 
   {
     id : 5,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter half moon shoulder bag(sage green)",
+    content : "•Detail : 포터의 반달 하프문 숄더백 세이지 그린 색상입니다. 특별한 하자 없으며 전체적으로 상태 우수합니다. •Size : 가로34 세로20",
     price : 130000
   },
   
   {
     id : 6,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter 80 anniversary tanker shoulder bag",
+    content : "•Detail : 포터의 80주년을 기념하여 한정 발매 된 오렌지 탱커백 입니다. 희귀한 매물이며 전체적으로 상태 양호하나, 새제품이 아니다 보니 전체적으로 조금씩 때가 타 있는 흔적들이 있습니다 이 점 고려부탁드립니다. •Size : 가로26 세로18",
     price : 130000
   },
 
   {
     id : 7,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker shoulder bag (olive)",
+    content : "•Detail : 요시다 포터의 탱커 숄더백 카키색상입니다. 특별한 하자 없는 우수한 컨디션 입니다. •Size : 가로26 세로18",
     price : 130000
   },
 
   {
     id : 8,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker 2-way boston bag",
+    content : "•Detail : 포터의 보스턴백입니다. 수납력 우수하고, 전체적으로 상태 우수합니다.•Size : 가로44 세로30",
     price : 130000
   },
 
   {
     id : 9,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker 2-way brief case bag",
+    content : "•Detail : 포터의 보스턴백입니다. 수납력 우수하고, 전체적으로 상태 우수합니다.•Size : 가로50 세로37",
     price : 130000
   },
 
   {
     id : 10,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter padded shoulder bag(black)",
+    content : "•Detail : 포터의 블랙 숄더백입니다. 패딩백 재질이며 전체적으로 상태 우수합니다. 이전 패딩백과 동일모델 다른 사이즈입니다. •Size : 가로43 세로28",
     price : 130000
   },
 
   {
     id : 11,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter padded shoulder bag(black)",
+    content : "•Detail : 포터의 블랙 숄더백입니다. 패딩백 재질이며 전체적으로 상태 우수합니다.•Size : 가로34 세로20",
     price : 130000
   },
 
   {
     id : 12,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter boston jet bag(jet silver)",
+    content : "•Detail : 포터의 해드포터라인의 jet silver 컬러 가방입니다. 자연스러운 사용감 존재하지만 특별한 하자없는 우수한 컨디션입니다. •Size : 가로23 세로15",
     price : 130000
   },
 
   {
     id : 13,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter freestyle shoulder bag(black)",
+    content : "•Detail : 포터의 프리스타일 숄더백입니다. 자연스러운 사용감 존재하나 전체적으로 우수한 컨디션 입니다. •Size : 가로27 세로17",
     price : 130000
   },
 
   {
     id : 14,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker bag(balck)",
+    content : "•Detail : 포터의 탱커백입니다. 자연스러운 사용감 존재하고 마지막 사진에서 확인할 수 있는 미세 찢어짐 존재합니다. •Size : 가로30 세로23",
     price : 130000
   },
 
   {
     id : 15,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker 2-way brief case bag",
+    content : "•Detail : 포터의 보스턴백입니다. 자연스러운 사용감 존재하고 마지막 사진 2개에서 확인할 수 있는 등의 미세 찢어짐 몇 군데 좀재합니다. •Size : 가로46 세로36",
     price : 130000
   },
 
   {
     id : 16,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter 70th anniversary tote bag",
+    content : "•Detail : 포터의 70주년 한정발매 토트백입니다. 전채적으로 자연스러운 사용감 존재합니다. •Size : 가로37 세로25",
     price : 130000
   },
   
   {
     id : 17,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker boston canvas bag",
+    content : "•Detail : 포터의 보스턴백입니다. 소재특성상 워싱 등의 자연스러운 사용감 존재하나 특별한 하자 없습니다. •Size : 가로30 세로17",
     price : 130000
   },
   
   {
     id : 18,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter coin case pink",
+    content : "•Detail : 포터의 카드지갑으로 사용 가능한 코인케이스 입니다. 자연스러운 사용감 존재합니다. 내부 오염존재하지만 개인적으로 사용하는데 불편함은 없다고 생각합니다. •Size : 가로12 세로8",
     price : 130000
   },
   
   {
     id : 19,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker boston bag (black)",
+    content : "•Detail : 포터의 보스턴백입니다. 자연스러운 사용감 존재하나 특별한 하자 없습니다. •Size : 가로36 세로26",
     price : 130000
   },
   
   {
     id : 20,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl glitter shoulder bag(sliver)",
+    content : "•Detail : 은은한 광택감이 도는 포터사의 숄더백입니다. 오염이나 하자 없는 우수한 컨디션 입니다. •Size : 가로38 세로23s",
     price : 130000
   },
   
   {
     id : 21,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker boston hand bag (black)",
+    content : "•Detail : 포터의 보스턴백입니다. 특별한 하자나 오염없는 우수한 컨디션 입니다. •Size : 가로34 세로22",
     price : 130000
   },
   
   {
     id : 22,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker boston bag",
+    content : "•Detail : 포터의 보스턴백입니다. 특별한 하자 없으나 자연스러운 사용감 존재합니다. •Size : 가로36 세로20",
     price : 130000
   },
   
   {
     id : 23,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter x sac’s coin case",
+    content : "•Detail : 포터의 카드지갑으로 사용 가능한 코인케이스 입니다. 전체적으로 양호한 상태일뿐만 아니라 사용감 전혀 없는 데드스탁급 제품입니다. •Size : 가로12 세로8",
     price : 130000
   },
   
   {
     id : 24,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl bonbon shoulder bag s(black)",
+    content : "•Detail : 애나멜로 되어있는 포터의 숄더백입니다. •Size : 가로29 세로15",
     price : 130000
   },
   
   {
     id : 25,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter freestyle shoulder bag s(camel)",
+    content : "•Detail : 포터의 프리스타일 숄더백 카멜컬러입니다. •Size : 가로33 세로18",
     price : 130000
   },
   
   {
     id : 26,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl bonbon shoulder bag M(purple)",
+    content : "•Detail : 애나멜로 되어있는 포터의 숄더백입니다. •Size : 가로33 세로18",
     price : 130000
   },
   
   {
     id : 27,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker boston bag (black)",
+    content : "•Detail : 포터의 보스턴백입니다. 특별한 하자 없으나 자연스러운 사용감 존재합니다. 마지막사진 확인부탁드립니다. •Size : 가로35 세로15",
     price : 130000
   },
   
   {
     id : 28,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter tanker shoulder bag (olive)",
+    content : "•Detail : 요시다 포터의 탱커 숄더백 카키색상입니다. 특별한 하자 없는 우수한 컨디션 입니다. •Size : 가로29 세로15",
     price : 130000
   },
   
   {
     id : 29,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter padded shoulder(beige)",
+    content : "•Detail : 포터의 베이지색 숄더백입니다. 패딩백 재질이며 전면 부 오염 존재합니다. 마지막 사진에서 확인 부탁드립니다. 감안하여 가격 책정하였습니다. •Size : 가로36 세로25",
     price : 130000
   },
   
   {
     id : 30,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl bonbon shoulder bag s(black)",
+    content : "•Detail : 애나멜로 되어있는 포터의 숄더백입니다. •Size : 가로27 세로12",
     price : 130000
   },
   
   {
     id : 31,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter half moon shoulder bag",
+    content : "•Detail : 포터의 반달 하프문 숄더백 세이지 블랙 색상입니다. 특별한 하자 없으며 전체적으로 상태 우수합니다. •Size : 가로34 세로20",
     price : 130000
   },
   
   {
     id : 32,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter freestyle shoulder bag s(camel)",
+    content : "•Detail : 포터의 프리스타일 숄더백 카멜컬러입니다. 전체적으로 양호한 상태이지만 가방 뒷면부에 자세히 보면 때가 타 있는 듯한 부분이 있습니다. 개인적으로 착용하시는데 큰 문제가 되지 않는다고 생각하지만 예민하신 분들은 신중한 구매 부탁드립니다 •Size : 가로33 세로18",
     price : 130000
   },
   
   {
     id : 33,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl bonbon shoulder bag m(black)",
+    content : "•Detail : 애나멜로 되어있는 포터사의 숄더백입니다 전체적으로 양호한 상태이나 마지막 사진에서 확인할 수 있는 훼손 부분 확인 부탁드립니다 •Size : 가로33 세로18",
     price : 130000
   },
   
   {
     id : 34,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl bonbon shoulder bag m(black)",
+    content : "•Detail : 애나멜로 되어있는 포터사의 숄더백입니다 전체적으로 양호한 상태이나 마지막 사진에서 확인할 수 있는 훼손 부분 확인 부탁드립니다 •Size : 가로33 세로18",
     price : 130000
   },
   
   {
     id : 35,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "Porter girl glitter shoulder bag(sliver)",
+    content : "•Detail : 은은한 광택감이 도는 포터사의 숄더백입니다 전체적으로 양호한 상태이나 뒷면에 마지막 사진에서 확인할 수 있는 미세한 오염 존재하며 아주 약간의 끈적임이 있습니다 개인적으로 착용과 사용에 전혀 문제 없는 수준이라고 판단되지만 민감하신분들은 신중한 구매 부탁드립니다 •Size : 가로30 세로20",
     price : 130000
   },
   
   {
     id : 36,
-    title : "porter half bag[olive]",
-    content : "Born in the States",
+    title : "porter cire enamel backpack",
+    content : "•Size : 가로28 세로41",
     price : 130000
   }
 ] 
