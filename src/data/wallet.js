9d0668cda90499ec8acf9e1db1014a311c1b1a5f
khwww <khw4420@knu.ac.kr>
2024-02-19 16:25:10 +0900

    대문 수정

---
+++ src/data/wallet.js
@@ -0,0 +1,81 @@
+let data = [ //대괄호니까 array
+//지금 하나의 array안에 6개의 object정보들이 들어가 있는 거임
+  {
+    id : 0,
+    title : "Porter girl bonbon shoulder bag s[black]",
+    content : "•Detail : 애나멜로 되어있는 포터의 숄더백입니다. •Size : 가로29 세로15",
+    price : 120000
+  },
+    
+  {
+    id : 27,
+    title : "Porter tanker boston bag (black)",
+    content : "•Detail : 포터의 보스턴백입니다. 특별한 하자 없으나 자연스러운 사용감 존재합니다. 마지막사진 확인부탁드립니다. •Size : 가로35 세로15",
+    price : 130000
+  },
+  
+  {
+    id : 28,
+    title : "Porter tanker shoulder bag (olive)",
+    content : "•Detail : 요시다 포터의 탱커 숄더백 카키색상입니다. 특별한 하자 없는 우수한 컨디션 입니다. •Size : 가로29 세로15",
+    price : 130000
+  },
+  
+  {
+    id : 29,
+    title : "Porter padded shoulder(beige)",
+    content : "•Detail : 포터의 베이지색 숄더백입니다. 패딩백 재질이며 전면 부 오염 존재합니다. 마지막 사진에서 확인 부탁드립니다. 감안하여 가격 책정하였습니다. •Size : 가로36 세로25",
+    price : 130000
+  },
+  
+  {
+    id : 30,
+    title : "Porter girl bonbon shoulder bag s(black)",
+    content : "•Detail : 애나멜로 되어있는 포터의 숄더백입니다. •Size : 가로27 세로12",
+    price : 130000
+  },
+  
+  {
+    id : 31,
+    title : "Porter half moon shoulder bag",
+    content : "•Detail : 포터의 반달 하프문 숄더백 세이지 블랙 색상입니다. 특별한 하자 없으며 전체적으로 상태 우수합니다. •Size : 가로34 세로20",
+    price : 130000
+  },
+  
+  {
+    id : 32,
+    title : "Porter freestyle shoulder bag s(camel)",
+    content : "•Detail : 포터의 프리스타일 숄더백 카멜컬러입니다. 전체적으로 양호한 상태이지만 가방 뒷면부에 자세히 보면 때가 타 있는 듯한 부분이 있습니다. 개인적으로 착용하시는데 큰 문제가 되지 않는다고 생각하지만 예민하신 분들은 신중한 구매 부탁드립니다 •Size : 가로33 세로18",
+    price : 130000
+  },
+  
+  {
+    id : 33,
+    title : "Porter girl bonbon shoulder bag m(black)",
+    content : "•Detail : 애나멜로 되어있는 포터사의 숄더백입니다 전체적으로 양호한 상태이나 마지막 사진에서 확인할 수 있는 훼손 부분 확인 부탁드립니다 •Size : 가로33 세로18",
+    price : 130000
+  },
+  
+  {
+    id : 34,
+    title : "Porter girl bonbon shoulder bag m(black)",
+    content : "•Detail : 애나멜로 되어있는 포터사의 숄더백입니다 전체적으로 양호한 상태이나 마지막 사진에서 확인할 수 있는 훼손 부분 확인 부탁드립니다 •Size : 가로33 세로18",
+    price : 130000
+  },
+  
+  {
+    id : 35,
+    title : "Porter girl glitter shoulder bag(sliver)",
+    content : "•Detail : 은은한 광택감이 도는 포터사의 숄더백입니다 전체적으로 양호한 상태이나 뒷면에 마지막 사진에서 확인할 수 있는 미세한 오염 존재하며 아주 약간의 끈적임이 있습니다 개인적으로 착용과 사용에 전혀 문제 없는 수준이라고 판단되지만 민감하신분들은 신중한 구매 부탁드립니다 •Size : 가로30 세로20",
+    price : 130000
+  },
+  
+  {
+    id : 36,
+    title : "porter cire enamel backpack",
+    content : "•Size : 가로28 세로41",
+    price : 130000
+  }
+] 
+
+export default data;
